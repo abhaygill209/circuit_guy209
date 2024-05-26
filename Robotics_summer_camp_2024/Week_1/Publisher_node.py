f321c0e29e11a8e74e4d1a12cb98dc5bff563d9c
abhaygill209 <146229446+abhaygill209@users.noreply.github.com>
2024-05-26 20:46:11 +0530

    Update Publisher_node.py

--- Robotics_summer_camp_2024/Week_1/Publisher_node.py
+++ Robotics_summer_camp_2024/Week_1/Publisher_node.py
@@ -4,23 +4,30 @@
 
 
 #!/usr/bin/env python3
-
-import rospy 
-from std_msgs.msg import Int32 
+import rospy
+from std_msgs.msg import String
 
 def talker():
-	pub = rospy.Publisher('chatter', Int32, queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(10)
-	i = 0
-	while not rospy.is_shutdown():
-		number = i
-		i=i+1
-		rospy.loginfo(number)
-		pub.publish(number)
-		rate.sleep()
+    # Initialize the node with the name 'talker'
+    rospy.init_node('talker', anonymous=True)
+    # Create a publisher object with the topic 'chatter' and message type 'String'
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    # Set the loop rate to 1 Hz
+    rate = rospy.Rate(1) # 1 Hz
+
+    while not rospy.is_shutdown():
+        # Create a hello_str message
+        hello_str = "hello world %s" % rospy.get_time()
+        # Log the message to the console
+        rospy.loginfo(hello_str)
+        # Publish the message to the 'chatter' topic
+        pub.publish(hello_str)
+        # Sleep to maintain the loop rate
+        rate.sleep()
+
 if __name__ == '__main__':
-	try:
-		talker()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
