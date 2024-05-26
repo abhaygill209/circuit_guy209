9964e7b4d14da912247faa54cdbb6ecf8012418a
abhaygill209 <146229446+abhaygill209@users.noreply.github.com>
2024-05-26 20:52:51 +0530

    Update Subscriber_node.py

--- Robotics_summer_camp_2024/Week_1/Subscriber_node.py
+++ Robotics_summer_camp_2024/Week_1/Subscriber_node.py
@@ -7,7 +7,7 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo("Communication is working smoothly! Ping no. %s", data.data)
+    rospy.loginfo("I heard %s", data.data)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
