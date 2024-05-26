4c57fe77f78b3cb56c4358e53ee457103b980263
abhaygill209 <146229446+abhaygill209@users.noreply.github.com>
2024-05-26 20:47:41 +0530

    Update Publisher_node.py

--- Robotics_summer_camp_2024/Week_1/Publisher_node.py
+++ Robotics_summer_camp_2024/Week_1/Publisher_node.py
@@ -8,21 +8,14 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    # Initialize the node with the name 'talker'
     rospy.init_node('talker', anonymous=True)
-    # Create a publisher object with the topic 'chatter' and message type 'String'
     pub = rospy.Publisher('chatter', String, queue_size=10)
-    # Set the loop rate to 1 Hz
-    rate = rospy.Rate(1) # 1 Hz
+    rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
-        # Create a hello_str message
-        hello_str = "hello world %s" % rospy.get_time()
-        # Log the message to the console
-        rospy.loginfo(hello_str)
-        # Publish the message to the 'chatter' topic
-        pub.publish(hello_str)
-        # Sleep to maintain the loop rate
+        com_str = "Communication is working smoothly! Ping no. %s" % rospy.get_time()
+        rospy.loginfo(com_str)
+        pub.publish(com_str)
         rate.sleep()
 
 if __name__ == '__main__':
