e97bde0ae0e0a50731880c966394f2ea611cca8e
abhaygill209 <146229446+abhaygill209@users.noreply.github.com>
2025-01-09 23:31:58 +0500

    Update and rename phase_modulation.py to low_pass_filter.py

--- Audio processing/low_pass_filter.py
+++ Audio processing/low_pass_filter.py
@@ -3,7 +3,7 @@ import math
 from pydub import AudioSegment
 import numpy as np
 import matplotlib.pyplot as plt
-samplerate,data= scipy.io.wavfile.read('skyfall_clip.wav')
+samplerate,data= scipy.io.wavfile.read('/home/abhay/Desktop/EC-211(Analog Communication)/skyfall_clip.wav')
 
 def prepare_audio(filename, duration, start_time):
     samplerate, data = scipy.io.wavfile.read(filename)
@@ -13,25 +13,25 @@ def prepare_audio(filename, duration, start_time):
     plt.ylabel('Amplitude')
     plt.show()"""
     if(data.shape[1]==2):
-      mono_sound=AudioSegment.from_wav(filename)
-      mono_sound = mono_sound.set_channels(1)
-      mono_sound.export("/content/mono.wav", format="wav")
-      mono_samplerate,mono_data= scipy.io.wavfile.read('mono.wav')
+      left_channel = data[:, 0]
+      right_channel = data[:, 1]
+      mono_data = left_channel + right_channel / 2
+      mono_data = mono_data.astype(np.int16)
     #Normalization
       mono_data_M=mono_data.max()
       mono_data_m=mono_data.min()
       mono_data_N=(2/(mono_data_M-mono_data_m))*(mono_data-mono_data_M)+1
-      data=mono_data_N[start_time*mono_samplerate :duration*mono_samplerate]
+      data=mono_data_N[start_time*samplerate :duration*samplerate]
     else:
       data_M=mono_data.max()
       data_m=mono_data.min()
       data_N=(2/(mono_data_M-mono_data_m))*(mono_data-mono_data_M)+1
-      data=data_N[start_time*mono_samplerate :duration*mono_samplerate]
+      data=data_N[start_time*samplerate :duration*samplerate]
 
 
     return time,samplerate,data
 
-time,sr,mono_data=prepare_audio('skyfall_clip.wav',15,13)
+time,sr,mono_data=prepare_audio('/home/abhay/Desktop/EC-211(Analog Communication)/skyfall_clip.wav',15,13)
 
 plt.plot(time,mono_data,linewidth = '0.15')
 plt.xlabel('Time (s)')
